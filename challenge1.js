3e48a920ed0b71c3721a1a4ad0c525aee54dc82e
Banele-mofokeng <mofokengbanele9@gmail.com>
2023-03-22 15:51:28 +0200

    deleted john's BMI

--- challenge1.js
+++ challenge1.js
@@ -1,11 +1,6 @@
 let marksWeight = 78;
 let marksHeight = 1.69;
-let johnsWeight = 92;
-let johnsHeight = 1.95;
-
-let johnsBMI = johnsWeight / johnsHeight ** 2;
 
 let marksBMI = marksWeight / marksHeight ** 2;
 
-console.log(johnsBMI);
 console.log(marksBMI);
